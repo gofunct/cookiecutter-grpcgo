9571ef17962dfdb410f73f0c737fe1a0f105f79c
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-20 16:23:54 -0700

    makefile stuff

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@
 
 user: ## rebuild the example from scratch
 	rm -rf user
-	cookiecutter https://github.com/gofunct/cookiecutter-grpcgo.git
-	cd example; make setup
+	cookiecutter --no-input https://github.com/gofunct/cookiecutter-grpcgo.git
+	cd user; make setup
 
 help: ## help
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST) | sort
---
+++ user/services/user/transports/http/http.go
@@ -0,0 +1,79 @@
+package user_httptransport
+
+
+
+import (
+       "log"
+	"net/http"
+	"encoding/json"
+	"context"
+
+        pb "github.com/gofunct/cookiecutter-grpcgo/user/gen/user/pb"
+        gokit_endpoint "github.com/go-kit/kit/endpoint"
+        httptransport "github.com/go-kit/kit/transport/http"
+        endpoints "github.com/gofunct/cookiecutter-grpcgo/user/gen/user/endpoints"
+)
+
+var _ = log.Printf
+var _ = gokit_endpoint.Chain
+var _ = httptransport.NewClient
+
+
+
+	
+		func MakeCreateUserHandler(svc pb.UserServiceServer, endpoint gokit_endpoint.Endpoint) *httptransport.Server {
+			return httptransport.NewServer(
+				endpoint,
+				decodeCreateUserRequest,
+				encodeResponse,
+				[]httptransport.ServerOption{}...,
+			)
+		}
+
+		func decodeCreateUserRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+			var req pb.CreateUserRequest
+			if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+				return nil, err
+			}
+			return &req, nil
+		}
+	
+
+	
+		func MakeGetUserHandler(svc pb.UserServiceServer, endpoint gokit_endpoint.Endpoint) *httptransport.Server {
+			return httptransport.NewServer(
+				endpoint,
+				decodeGetUserRequest,
+				encodeResponse,
+				[]httptransport.ServerOption{}...,
+			)
+		}
+
+		func decodeGetUserRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+			var req pb.GetUserRequest
+			if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+				return nil, err
+			}
+			return &req, nil
+		}
+	
+
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+func RegisterHandlers(svc pb.UserServiceServer, mux *http.ServeMux, endpoints endpoints.Endpoints) error {
+	
+		
+			log.Println("new HTTP endpoint: \"/CreateUser\" (service=User)")
+			mux.Handle("/CreateUser", MakeCreateUserHandler(svc, endpoints.CreateUserEndpoint))
+		
+	
+		
+			log.Println("new HTTP endpoint: \"/GetUser\" (service=User)")
+			mux.Handle("/GetUser", MakeGetUserHandler(svc, endpoints.GetUserEndpoint))
+		
+	
+	return nil
+}
