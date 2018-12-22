79d42c35c667d623d27e81ecd43a7792d2f79978
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-21 19:51:07 -0700

    added db

--- {{cookiecutter.app_name}}/go.mod
+++ {{cookiecutter.app_name}}/go.mod
@@ -8,9 +8,13 @@ require (
 	github.com/aokoli/goutils v1.0.1 // indirect
 	github.com/ckaznocha/protoc-gen-lint v0.2.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gemnasium/migrate v1.4.1
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-kit/kit v0.8.0
 	github.com/go-logfmt/logfmt v0.4.0 // indirect
+	github.com/go-pg/pg v6.15.1+incompatible
+	github.com/go-playground/locales v0.12.1 // indirect
+	github.com/go-playground/universal-translator v0.16.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gofunct/templates v0.0.0-20181220201910-fd76c260d65d // indirect
 	github.com/gogo/protobuf v1.2.0
@@ -23,13 +27,17 @@ require (
 	github.com/huandu/xstrings v1.2.0 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/kr/fs v0.1.0 // indirect
+	github.com/leodido/go-urn v1.1.0 // indirect
+	github.com/lib/pq v1.0.0 // indirect
 	github.com/pseudomuto/protoc-gen-doc v1.1.0 // indirect
 	github.com/pseudomuto/protokit v0.1.0 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/viper v1.2.0
-	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9 // indirect
+	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9
 	golang.org/x/lint v0.0.0-20181217174547-8f45f776aaf1 // indirect
 	golang.org/x/net v0.0.0-20181220203305-927f97764cc3
 	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890 // indirect
@@ -39,6 +47,8 @@ require (
 	google.golang.org/appengine v1.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20181221175505-bd9b4fb69e2f // indirect
 	google.golang.org/grpc v1.17.0
+	gopkg.in/go-playground/validator.v9 v9.24.0
 	honnef.co/go/tools v0.0.0-20180920025451-e3ad64cb4ed3 // indirect
+	mellium.im/sasl v0.2.1 // indirect
 	moul.io/protoc-gen-gotemplate v1.6.0 // indirect
 )
---
+++ {{cookiecutter.app_name}}/store/db.go
@@ -0,0 +1,101 @@
+package store
+
+import (
+"errors"
+	validator "gopkg.in/go-playground/validator.v9"
+	"os"
+"time"
+"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	valid = validator.New()
+	ErrAccountNotFound = errors.New("account not found")
+	ErrEmailExists     = errors.New("email already exists")
+	ErrNoDatabase      = errors.New("no database connection details")
+	ErrNoPasswordGiven = errors.New("a password is required")
+)
+
+type Database interface {
+	List(count int32, token string) ([]*Account, string, error)
+	ReadByID(ID string) (*Account, error)
+	ReadByEmail(email string) (*Account, error)
+	Create(a *Account, password string) error
+	Update(a *Account) error
+	Delete(ID string) error
+	Confirm(token string) (*Account, error)
+	GeneratePasswordToken(email string) (*Account, error)
+	UpdatePassword(string, string) (*Account, error)
+	Migrate() error
+	Truncate() error
+	Close() error
+}
+
+type Account struct {
+	ID                 string `db:"id"`
+	Name               string `validate:"required"`
+	Email              string `validate:"required"`
+	HashedPassword     string `db:"hashed_password"`
+	ConfirmationToken  string
+	PasswordResetToken string
+	Metadata           map[string]string
+	CreatedAt          time.Time `db:"created_at"`
+}
+
+func (a *Account) Valid() error {
+	return valid.Struct(a)
+}
+
+func (a *Account) HashPassword(password string) error {
+	if password == "" {
+		return ErrNoPasswordGiven
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	a.HashedPassword = string(hash[:])
+
+	return nil
+}
+
+func (a *Account) ComparePasswordToHash(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(a.HashedPassword), []byte(password))
+}
+
+func EmailExists(db Database, a *Account) error {
+	a, err := db.ReadByEmail(a.Email)
+	if err != nil && err != ErrAccountNotFound {
+		return err
+	}
+
+	if a != nil {
+		return ErrEmailExists
+	}
+
+	return nil
+}
+
+func DatabaseFromEnv() Database {
+	var conn Database
+	var err error
+
+	pg := os.Getenv("POSTGRESQL_URL")
+	if pg != "" {
+		pgConn := &PostgreSQL{}
+		err = pgConn.Connect(pg)
+		conn = pgConn
+	}
+
+	if conn == nil {
+		panic(ErrNoDatabase)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	return conn
+}
