e2118c1eac3d447585b56c90e8e0b14d56389105
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-21 19:38:46 -0700

    .

--- {{cookiecutter.app_name}}/cmd/serve.go
+++ {{cookiecutter.app_name}}/cmd/serve.go
@@ -14,6 +14,6 @@ var serveCmd = &cobra.Command{
 	Use: "serve",
 	Short: "start a grpc and http server",
 	Run: func(cmd *cobra.Command, args []string) {
-		server.RunServer()
+		server.RunServer(defaultConfig, kitLog)
 	},
 }
